efa3a8d7e1ece0f134f45043cc0d5e1d6833be55
Juliet Yznaga <juliet.yznaga@duke.edu>
2019-07-04 17:03:43 +0200

    model error fixed

--- src/app/models/listing.spec.ts
+++ src/app/models/listing.spec.ts
@@ -2,6 +2,6 @@ import { Listing } from './listing';
 
 describe('Listing', () => {
   it('should create an instance', () => {
-    expect(new Listing()).toBeTruthy();
+    expect(new Listing(String, String, Number, String)).toBeTruthy();
   });
 });
